0b597f306647ee07992ca8b2d40ff13ea1b9a58f
Maxim Trekusov <trekusovmax@gmail.com>
2022-02-06 23:25:06 +0300

    changed edit form

---
+++ src/app/pages/editQuality.jsx
@@ -0,0 +1,33 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import EditForm from '../components/ui/editForm'
+
+const EditQualityPage = () => {
+  const [quality, setQuality] = useState(null)
+  const id = useParams().id
+  const qualityEndPoint = `http://localhost:4000/api/v1/quality/${id}`
+  const handleSubmit = (data) => {
+    axios
+      .put(qualityEndPoint, data)
+      .then((res) => console.log(res.data.content))
+  }
+
+  useEffect(async () => {
+    const { data } = await axios.get(qualityEndPoint)
+    setQuality(data.content)
+  }, [])
+
+  return (
+    <>
+      <h1>Edit Quality Page</h1>{' '}
+      {quality !== null ? (
+        <EditForm data={quality} onSubmit={handleSubmit} />
+      ) : (
+        'Loading...'
+      )}
+    </>
+  )
+}
+
+export default EditQualityPage
