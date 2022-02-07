544a0ef8a293a6080a127ee43973c12d3ae01957
Maxim Trekusov <trekusovmax@gmail.com>
2022-02-07 23:38:08 +0300

    added http servise

--- src/app/pages/editQuality.jsx
+++ src/app/pages/editQuality.jsx
@@ -1,20 +1,25 @@
-import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import EditForm from '../components/ui/editForm'
+import httpService from '../service/http.service'
 
 const EditQualityPage = () => {
   const [quality, setQuality] = useState(null)
   const id = useParams().id
   const qualityEndPoint = `http://localhost:4000/api/v1/quality/${id}`
-  const handleSubmit = (data) => {
-    axios
-      .put(qualityEndPoint, data)
-      .then((res) => console.log(res.data.content))
+
+  const handleSubmit = async (data) => {
+    try {
+      await httpService
+        .put(qualityEndPoint, data)
+        .then((res) => console.log(res.data.content))
+    } catch (error) {
+      console.log('Expected error')
+    }
   }
 
   useEffect(async () => {
-    const { data } = await axios.get(qualityEndPoint)
+    const { data } = await httpService.get(qualityEndPoint)
     setQuality(data.content)
   }, [])
 
